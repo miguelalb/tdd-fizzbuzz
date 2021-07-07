11a0bbbca4d810f9257e22ef8e25bb3ae748dc3c
Miguel Acevedo <44342378+miguelalb@users.noreply.github.com>
2021-07-07 07:02:07 -0400

    Green - Refactor extract isMultiple utility method

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,10 @@
 def fizzBuzz(value):
-    if (value % 3) == 0:
+    if isMultiple(value, 3):
         return "Fizz"
-    if (value % 5) == 0:
+    if isMultiple(value, 5):
         return "Buzz"
     return str(value)
+
+
+def isMultiple(value, mod):
+    return (value % mod) == 0
