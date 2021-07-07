44e38c65f9cee28acf0419df6f814d36e44393d3
Miguel Acevedo <44342378+miguelalb@users.noreply.github.com>
2021-07-07 07:04:45 -0400

    Green - Generalize to check multiple of 3 and 5

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
 def fizzBuzz(value):
     if isMultiple(value, 3):
+        if isMultiple(value, 5):
+            return "FizzBuzz"
         return "Fizz"
     if isMultiple(value, 5):
         return "Buzz"
