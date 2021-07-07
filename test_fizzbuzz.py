c446aebc4e3923c59a14a6654406673cd6fb40a9
Miguel Acevedo <44342378+miguelalb@users.noreply.github.com>
2021-07-07 06:45:25 -0400

    Fail - Get 2 when I pass in 2

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -2,9 +2,11 @@ import pytest
 
 from fizzbuzz import fizzBuzz
 
-def test_canCallFizzBuzz():
-    fizzBuzz(1)
 
 def test_returns1With1PassedIn():
     val = fizzBuzz(1)
     assert val == "1"
+
+def test_resturns2With2PassedIn():
+    val = fizzBuzz(2)
+    assert val == "2"
