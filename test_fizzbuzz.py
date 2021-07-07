85e423eb2254d34b40a8d45038e370bb03178bb8
Miguel Acevedo <44342378+miguelalb@users.noreply.github.com>
2021-07-07 06:50:06 -0400

    Refactor - Remove duplicate structure in test

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -3,10 +3,13 @@ import pytest
 from fizzbuzz import fizzBuzz
 
 
+def checkFizzBuzz(value, expected):
+    val = fizzBuzz(value)
+    assert val == expected
+
+
 def test_returns1With1PassedIn():
-    val = fizzBuzz(1)
-    assert val == "1"
+    checkFizzBuzz(1, "1")
 
 def test_resturns2With2PassedIn():
-    val = fizzBuzz(2)
-    assert val == "2"
+    checkFizzBuzz(2, "2")
